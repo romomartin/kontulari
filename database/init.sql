7aa5b20b618dd0c7ea9cf58a11f0445fa474a1b1
romomartin <romoander@gmail.com>
2025-02-15 14:52:45 +0100

    Create expenses table schema

--- database/init.sql
+++ database/init.sql
@@ -1,7 +1,13 @@
 CREATE DATABASE IF NOT EXISTS test;
 USE test;
-CREATE TABLE IF NOT EXISTS test_table (
-    id int NOT NULL AUTO_INCREMENT,
-    valor int,
-    UNIQUE KEY (id)
+CREATE TABLE IF NOT EXISTS expenses (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    creation_timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    bank_account VARCHAR(255) NOT NULL,
+    accounting_date DATE NOT NULL,
+    value_date DATE NOT NULL,
+    og_description VARCHAR(255) NOT NULL,
+    amount FLOAT NOT NULL,
+    balance FLOAT NOT NULL,
+    `description` VARCHAR(255)
 )
